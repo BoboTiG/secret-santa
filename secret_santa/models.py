3e3f78613648cd94eeac9918ab987d444458e4de
Mickaël Schoentgen <contact@tiger-222.fr>
2022-11-07 13:20:22 +0100

    model: ensure mandatory data are set, and valid

--- secret_santa/models.py
+++ secret_santa/models.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from typing import List
 
 
@@ -9,6 +9,14 @@ class Event:
     sender: str
     email: str
 
+    def __post_init__(self) -> None:
+        """Ensure mandatory fields are set, and with proper data."""
+        for field in fields(self):
+            if not getattr(self, field.name):
+                raise ValueError(f"Event is missing the {field.name!r}!")
+        if "@" not in self.email:
+            raise ValueError(f"Invalid 'email': {self.email!r}!")
+
 
 @dataclass(slots=True)
 class Person:
@@ -17,6 +25,16 @@ class Person:
     email: str
     wishes: List[str]
     buddy: str
+
+    def __post_init__(self) -> None:
+        """Ensure mandatory fields are set, and with proper data."""
+        for attr in {"name", "nature", "email"}:
+            if not getattr(self, attr):
+                raise ValueError(f"{self} is missing the {attr!r}!")
+        if "@" not in self.email:
+            raise ValueError(f"Invalid 'email': {self.email!r}!")
+        if self.nature not in {"maman", "papa"}:
+            raise ValueError(f"Invalid 'nature': {self.nature!r}!")
 
 
 People = List[Person]
